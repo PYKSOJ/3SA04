b4cc0cdc5a64333ea7a6961a25a763251e2b937c
PYKSOJ <62101102166@psu.ac.th>
2021-07-15 21:54:35 +0700

    No.4 Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ImageBackground, Text, StyleSheet, View } from "react-native";
 import Forecast from "./Forecast";
 
@@ -8,6 +8,24 @@ export default function Weather(props) {
     description: "-",
     temp: 0,
   });
+
+  useEffect(() => {
+    console.log(`fetching data with zipCode = ${props.zipCode}`)
+    if (props.zipCode) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=d75c79fa238313cc4671656d19c60e08`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                });
+            })
+            .catch((error) => {
+                console.warn(error);
+            });
+    }
+}, [props.zipCode])
 
   return (
     <View>
