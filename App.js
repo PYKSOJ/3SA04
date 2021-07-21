0b68f2cde22c2860ef24993aad5e5a079c7a6995
PYKSOJ <62101102166@psu.ac.th>
2021-07-21 22:50:46 +0700

    config header

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
 import ZipCodeScreen from "./screens/ZipCodeScreen";
 import WeatherScreen from "./screens/WeatherScreen";
 
@@ -10,9 +13,30 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={ZipCodeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={ZipCodeScreen}
+          options={{
+            title: "Select Location",
+            headerStyle: {
+              backgroundColor: "#fe0000",
+            },
+            headerTintColor: "#FE0",
+            headerTitleStyle: {
+              alignItems: "center",
+              fontWeight: "bold",
+              alignSelf: "center",
+            },
+          }}
+        />
         <Stack.Screen name="Weather" component={WeatherScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+  },
+});
